0f18bff57dc45f1721b21e5976734bbe010fa62e
= <=>
2025-02-02 22:16:51 +0100

    version 4

--- tennis-leer.py
+++ tennis-leer.py
@@ -5,3 +5,7 @@ import numpy as np
 data = pd.read_csv("tennis.csv")
 play = data['play']
 plt.hist(play)
+plt.title("Play tennis")
+plt.xlabel("Play?")
+plt.ylabel("Frecuencia")
+plt.savefig("histograma_play.png")
